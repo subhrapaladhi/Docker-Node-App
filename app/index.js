075878203938f8c0bae4ae587f3f9530baa598a7
subhrapaladhi <subhrapaladhi9@gmail.com>
2020-07-27 17:02:08 +0530

    some small changes

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 const app = require("express")();
 const PORT = process.env.APPID||9999;
 
-app.get("/", (req, res) => res.send(`hello from a lightweight container. App ID = ${PORT}`))
+app.get("/", (req, res) => res.send(`hello from a lightweight container. App ID = ${PORT}\n`))
 
-app.get("/admin", (req, res) => res.send(`admin page. App ID = ${PORT}`))
+app.get("/admin", (req, res) => res.send(`admin page. App ID = ${PORT}\n`))
 
-app.get("/app1", (req, res) => res.send(`this is app1. App ID = ${PORT}`))
+app.get("/app1", (req, res) => res.send(`this is app1. App ID = ${PORT}\n`))
 
-app.get("/app2", (req, res) => res.send(`this is app2. App ID = ${PORT}`))
+app.get("/app2", (req, res) => res.send(`this is app2. App ID = ${PORT}\n`))
 
 app.listen(9999, ()=>console.log(`server started at port ${PORT}`))
